197985762c63c85af13da7e818daab993ec8b81e
StudentFPW <jofeleonids00@gmail.com>
2025-06-13 21:44:47 +0300

    deleted

--- auth/accounts/urls.py
+++ auth/accounts/urls.py
@@ -1,26 +1,6 @@
 from django.urls import path
 
-from .viewsets import VolunteerViewSet, OrganizationViewSet, AuthViewSet
+from .viewsets import *
 
 urlpatterns = [
-    path(
-        "volunteer/register/",
-        VolunteerViewSet.as_view({"post": "create"}),
-        name="volunteer-register",
-    ),
-    path(
-        "organization/register/",
-        OrganizationViewSet.as_view({"post": "create"}),
-        name="organization-register",
-    ),
-    path(
-        "auth/login/",
-        AuthViewSet.as_view({"post": "login"}),
-        name="login",
-    ),
-    path(
-        "auth/logout/",
-        AuthViewSet.as_view({"post": "logout"}),
-        name="logout",
-    ),
 ]
